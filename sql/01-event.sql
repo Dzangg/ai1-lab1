3a4576d827f158796986a0ee043befe3d46754e0
Bartlomiej Bielecki <bartlomiej.bielecki36@gmail.com>
2023-12-07 22:33:26 +0100

    fixes

--- sql/01-event.sql
+++ sql/01-event.sql
@@ -7,3 +7,12 @@ create table event
     content text not null,
     date text not null
 );
+
+create table post
+(
+    id      integer not null
+        constraint post_pk
+            primary key autoincrement,
+    subject text not null,
+    content text not null
+);
--- templates/nav.html.php
+++ templates/nav.html.php
@@ -4,6 +4,7 @@
 ?>
 <ul>
     <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
+    <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
     <li><a href="<?= $router->generatePath('event-index') ?>">Events</a></li>
 </ul>
 <?php
