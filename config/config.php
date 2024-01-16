928c53de2974258446d15b07bbd9a540ef4da1a8
Bartlomiej Bielecki <bartlomiej.bielecki36@gmail.com>
2024-01-16 15:41:38 +0100

    cordova

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config = [];
-$config['db_dsn'] = 'sqlite:C:\Users\dzang\Documents\Studia\5 semestr\aplikacje internetowe 1\L6\custom-php-framework\data.db';
-$config['db_user'] = '';
-$config['db_pass'] = '';
